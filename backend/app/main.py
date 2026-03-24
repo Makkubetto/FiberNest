8b818eade12f3595c88d1cec5a310476b170b1ab
Makkubetto <juliusdeleon26@gmail.com>
2026-03-24 20:15:17 +0800

    fix cors for vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,13 +24,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-                   "http://localhost:5500",
-                   "http://127.0.0.1:5500",
-                   "http://localhost:8000",
-                   "http://127.0.0.1:8000",
-                   "null",
-                   ],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
